ca3aec68112435a95a6f1381bfde0356b30e0c5f
Rijn, RA van - SPLXL <Ronald-van.Rijn@klm.com>
2013-08-09 13:25:45 +0200

    Added masking of passwords in the json payloads

--- src/main/java/com/github/styx/logging/MaskedPatternLayout.java
+++ src/main/java/com/github/styx/logging/MaskedPatternLayout.java
@@ -3,6 +3,9 @@ package com.github.styx.logging;
 import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 
+/**
+ * A logback pattern layout that performs masking of passwords.
+ */
 public class MaskedPatternLayout extends PatternLayout {
 
     @Override
@@ -14,11 +17,36 @@ public class MaskedPatternLayout extends PatternLayout {
         return message;
     }
 
-    private String maskPasswords(String message) {
-        int startIndex = message.indexOf("password=[");
-        if (startIndex > 0) {
-            int endIndex = message.indexOf("]", startIndex);
-            message = message.replace(message.substring(startIndex, endIndex + 1), "password=[********]");
+    /**
+     * Mask all password types in the given message.
+     *
+     * @param message  the message to mask the passwords in
+     *
+     * @return a message with all passwords masked
+     */
+    protected String maskPasswords(String message) {
+        if (message.contains("password")) {
+            message = maskPassword(message, "password=[", "]");
+            message = maskPassword(message, "\"password\":\"", "\"");
+        }
+        return message;
+    }
+
+    /**
+     * Mask a password in the given message that starts with the given start token and
+     * ends with the end token.
+     *
+     * @param message     the message to mask the password in
+     * @param startToken  the start token of the password
+     * @param endToken    the end token of the password
+     *
+     * @return a message with the password masked
+     */
+    private String maskPassword(String message, String startToken, String endToken) {
+        int startIndex = message.indexOf(startToken);
+        if (startIndex > -1) {
+            int endIndex = message.indexOf(endToken, startIndex + startToken.length());
+            message = message.replace(message.substring(startIndex, endIndex + 1), startToken.concat("********").concat(endToken));
         }
         return message;
     }
---
+++ src/test/java/com/github/styx/logging/MaskedPatternLayoutTest.java
@@ -0,0 +1,29 @@
+package com.github.styx.logging;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MaskedPatternLayoutTest {
+
+    private MaskedPatternLayout maskedPatternLayout = new MaskedPatternLayout();
+
+    @Test
+    public void testMaskPasswordsShouldFailWhenToStringPasswordIsNotReplaced() {
+        String message = maskedPatternLayout.maskPasswords("password=[pswpsw123]");
+        assertEquals("Expected message to be replaced", "password=[********]", message);
+    }
+
+    @Test
+    public void testMaskPasswordsShouldFailWhenJsonPasswordIsNotReplaced() {
+        String message = maskedPatternLayout.maskPasswords("\"password\":\"pswpsw123\"");
+        assertEquals("Expected message to be replaced", "\"password\":\"********\"", message);
+    }
+
+    @Test
+    public void testMaskPasswordsShouldFailWhenPasswordLiteralIsReplaced() {
+        String message = maskedPatternLayout.maskPasswords("\"grant_type\":\"password\"");
+        assertEquals("Expected message to not be replaced", "\"grant_type\":\"password\"", message);
+    }
+
+}
