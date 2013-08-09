1c9ed5706ed276a7f4a497af24ef89f869c33e62
Rijn, RA van - SPLXL <Ronald-van.Rijn@klm.com>
2013-08-09 13:39:34 +0200

    Better error and success handling for registration page

--- src/main/webapp/web-resources/js/styx-controllers.js
+++ src/main/webapp/web-resources/js/styx-controllers.js
@@ -26,9 +26,12 @@ styxControllers.controller('LoginController', function ($scope, cloudfoundry, $l
 
 styxControllers.controller('RegisterController', function($scope, cloudfoundry, $location) {
     $scope.register = function(userForm) {
+        $scope.success = '';
+        $scope.error = '';
+
         var registrationPromise = cloudfoundry.register(userForm);
         registrationPromise.success(function (data, status, headers) {
-            $location.path("/login");
+            $scope.success = 'Registration successful! Your user is active, ask an organization manager to add you to an organization.';
         });
         registrationPromise.error(function (data, status, headers) {
             $scope.error = 'Registration failed. Reason: ' + data.message;
