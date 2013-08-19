8d4e6bccfc5ab67c3b77e71e1ef8e0f00490be54
Rijn, RA van - SPLXL <Ronald-van.Rijn@klm.com>
2013-08-19 15:33:15 +0200

    Javadoc for access token

--- src/main/java/com/github/styx/domain/AccessToken.java
+++ src/main/java/com/github/styx/domain/AccessToken.java
@@ -5,6 +5,9 @@ import org.apache.commons.lang.builder.ToStringStyle;
 
 import static org.mvel2.MVEL.evalToString;
 
+/**
+ * An OAuth access token needed to communicate with the API.
+ */
 public class AccessToken {
 
     private final String accessToken;
@@ -15,35 +18,78 @@ public class AccessToken {
 
     private String username;
 
+    /**
+     * Constructs a new access token.
+     *
+     * @param accessToken  the token value
+     * @param tokenType    the token type
+     */
     public AccessToken(String accessToken, String tokenType) {
         this.accessToken = accessToken;
         this.tokenType = tokenType;
     }
 
+    /**
+     * Returns the token value.
+     *
+     * @return the token value
+     */
     public String getAccessToken() {
         return accessToken;
     }
 
+    /**
+     * Returns the token type.
+     *
+     * @return the token type
+     */
     public String getTokenType() {
         return tokenType;
     }
 
+    /**
+     * Returns the id of the user the token belongs to.
+     *
+     * @return the id of the user
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Returns the user name of the user the token belongs to.
+     *
+     * @return the user name of the user
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Set the user id of the user the token belongs to.
+     *
+     * @param id  the id of the user
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Sets the user name of the user the token belongs to.
+     *
+     * @param username  the user name of the user
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Constructs a new access token from the given API response.
+     *
+     * @param response  the response to create the access token from
+     *
+     * @return a new access token
+     */
     public static AccessToken fromCloudFoundryModel(Object response) {
         return new AccessToken(evalToString("access_token", response), evalToString("token_type", response));
     }
